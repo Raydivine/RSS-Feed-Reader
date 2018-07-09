9ea6c902f324eaaa6082bb2d6a43ea10c7126e71
Raydivine <raysternwong@gmail.com>
2018-07-09 22:07:02 +0800

    Created a method and Test Project

---
+++ RssFeedReader/RssFeedReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace RssFeedReader
+{
+    public struct News
+    {
+        string title;
+        string story;
+    }
+
+    public class RssFeedReader
+    {
+        public RssFeedReader() { }
+
+        public List<News> getNewsFromRssURL(string rssUrl)
+        {
+            List<News> newsList = new List<News>();
+
+            return newsList;
+        }
+    }
+}
---
+++ TestRssFeedReader/testGetNewsFromRssURL.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RssFeedReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssFeedReader.Tests
+{
+    [TestClass()]
+    public class testGetNewsFromRssURL
+    {
+        [TestMethod()]
+        public void getNewsFromRssURLTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
