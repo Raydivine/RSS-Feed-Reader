3a751fe80778db7cd7f90d2eac67148bf1ed3a55
Raydivine <raysternwong@gmail.com>
2018-07-09 21:56:07 +0800

    Recreated the class library project

--- RssFeedReader/Class1.cs
+++ RssFeedReader/Class1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RssFeedReader
 {
-    public class Class1
+    public class RssFeedReader
     {
     }
 }
